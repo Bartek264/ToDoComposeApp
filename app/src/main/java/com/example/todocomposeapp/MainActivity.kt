36f3a282e868dbcc0dde7ac67dd5b5903b436a99
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-24 19:37:08 +0100

    Commit

--- app/src/main/java/com/example/todocomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/todocomposeapp/MainActivity.kt
@@ -3,21 +3,21 @@ package com.example.todocomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.todocomposeapp.navigation.SetupNavigation
 import com.example.todocomposeapp.ui.theme.ToDoComposeAppTheme
 
 class MainActivity : ComponentActivity() {
+
+	private lateinit var navController: NavHostController
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
 			ToDoComposeAppTheme {
-
+				navController = rememberNavController()
+				SetupNavigation(navController = navController)
 			}
 		}
 	}

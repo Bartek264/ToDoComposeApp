05c9a41a2b29a533c7d0975be6093e743da730b3
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-25 00:40:27 +0100

    TopAppBar

--- app/src/main/java/com/example/todocomposeapp/ui/component/ListTopBar.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/component/ListTopBar.kt
@@ -7,6 +7,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todocomposeapp.R
+import com.example.todocomposeapp.ui.theme.topAppBarBackgroundColor
+import com.example.todocomposeapp.ui.theme.topAppBarContentColor
 
 @Composable
 fun ListTopBar() {
@@ -17,7 +19,10 @@ fun ListTopBar() {
 @Composable
 fun DefaultTopBar() {
 	TopAppBar(title = {
-		Text(text = stringResource(id = R.string.list_top_bar_title))
-	}, backgroundColor = MaterialTheme.colors.primary)
+		Text(
+			text = stringResource(id = R.string.list_top_bar_title),
+			color = MaterialTheme.colors.topAppBarContentColor
+		)
+	}, backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor)
 
 }

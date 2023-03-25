261e0589db842374ac6601e19123b6e367cd60a3
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-25 19:04:01 +0100

    TopAppBar

--- app/src/main/java/com/example/todocomposeapp/ui/component/ListTopBar.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/component/ListTopBar.kt
@@ -1,25 +1,30 @@
 package com.example.todocomposeapp.ui.component
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todocomposeapp.R
 import com.example.todocomposeapp.data.model.Priority
+import com.example.todocomposeapp.ui.theme.Typography
 import com.example.todocomposeapp.ui.theme.topAppBarBackgroundColor
 import com.example.todocomposeapp.ui.theme.topAppBarContentColor
 
 @Composable
 fun ListTopBar() {
-	DefaultTopBar(onSearchClicked = {}, onSortAction = {})
+	DefaultTopBar(onSearchClicked = {}, onSortAction = {}, onDeleteClicked = {})
 }
 
 @Composable
-fun DefaultTopBar(onSearchClicked: () -> Unit, onSortAction: (Priority) -> Unit) {
+fun DefaultTopBar(onSearchClicked: () -> Unit, onSortAction: (Priority) -> Unit, onDeleteClicked: () -> Unit) {
 	TopAppBar(
 		title = {
 			Text(
@@ -28,15 +33,16 @@ fun DefaultTopBar(onSearchClicked: () -> Unit, onSortAction: (Priority) -> Unit)
 			)
 		}, backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
 		actions = {
-			ListAppBarAction(onSearchClicked, onSortAction)
+			ListAppBarAction(onSearchClicked, onSortAction, onDeleteClicked)
 		}
 	)
 }
 
 @Composable
-fun ListAppBarAction(onSearchClicked: () -> Unit, onSortAction: (Priority) -> Unit) {
+fun ListAppBarAction(onSearchClicked: () -> Unit, onSortAction: (Priority) -> Unit, onDeleteClicked: () -> Unit) {
 	SearchAction(onSearchClicked = onSearchClicked)
 	SortAction(onSortAction = onSortAction)
+	DeleteAllAction(onDeleteClicked = onDeleteClicked)
 }
 
 @Composable
@@ -44,7 +50,7 @@ fun SearchAction(onSearchClicked: () -> Unit) {
 	IconButton(onClick = { onSearchClicked() }) {
 		Icon(
 			imageVector = Icons.Filled.Search,
-			contentDescription = null,
+			contentDescription = stringResource(id = R.string.search),
 			tint = MaterialTheme.colors.topAppBarContentColor
 		)
 	}
@@ -58,7 +64,7 @@ fun SortAction(onSortAction: (Priority) -> Unit) {
 	IconButton(onClick = { expended = true }) {
 		Icon(
 			imageVector = ImageVector.vectorResource(id = R.drawable.filter_list),
-			contentDescription = null,
+			contentDescription = stringResource(id = R.string.filter),
 			tint = MaterialTheme.colors.topAppBarContentColor
 		)
 		DropdownMenu(expanded = expended,
@@ -83,8 +89,35 @@ fun SortAction(onSortAction: (Priority) -> Unit) {
 	}
 }
 
+@Composable
+fun DeleteAllAction(onDeleteClicked: () -> Unit) {
+
+	var expanded by remember { mutableStateOf(false) }
+
+	IconButton(onClick = { expanded = true }) {
+		Icon(
+			imageVector = ImageVector.vectorResource(id = R.drawable.dots_menu),
+			contentDescription = stringResource(id = R.string.delete_all),
+			tint = MaterialTheme.colors.topAppBarContentColor
+		)
+		DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+			DropdownMenuItem(onClick = {
+				expanded = false
+				onDeleteClicked()
+			}) {
+				Icon(imageVector = Icons.Filled.Delete, contentDescription = stringResource(id = R.string.delete_all))
+				Text(
+					modifier = Modifier.padding(start = 12.dp),
+					text = stringResource(id = R.string.delete_all),
+					style = Typography.subtitle1,
+				)
+			}
+		}
+	}
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewTopBar() {
-	DefaultTopBar(onSearchClicked = {}, onSortAction = {})
+	DefaultTopBar(onSearchClicked = {}, onSortAction = {}, onDeleteClicked = {})
 }

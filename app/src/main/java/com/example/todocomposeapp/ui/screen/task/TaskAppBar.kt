8d102b279a17b04b5bf91d5597f62684f0d3cfce
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-26 19:58:31 +0200

    TaskItem

--- app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskAppBar.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskAppBar.kt
@@ -2,11 +2,14 @@ package com.example.todocomposeapp.ui.screen.task
 
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.todocomposeapp.R
 import com.example.todocomposeapp.ui.component.ListAppBarAction
 import com.example.todocomposeapp.ui.theme.topAppBarBackgroundColor
@@ -31,7 +34,7 @@ fun NewTaskAppBar(
 			)
 		}, backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
 		actions = {
-
+			AddAction(onAddClick = navigationToListScreen)
 		}
 	)
 }
@@ -39,6 +42,19 @@ fun NewTaskAppBar(
 @Composable
 fun BackAction(onBackClick: (Action) -> Unit) {
 	IconButton(onClick = { onBackClick(Action.NO_ACTION) }) {
-		Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+		Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null, tint = MaterialTheme.colors.topAppBarContentColor)
 	}
+}
+
+@Composable
+fun AddAction(onAddClick: (Action) -> Unit) {
+	IconButton(onClick = { onAddClick(Action.ADD) }) {
+		Icon(imageVector = Icons.Filled.Check, contentDescription = null, tint = MaterialTheme.colors.topAppBarContentColor)
+	}
+}
+
+@Preview
+@Composable
+fun NewTaskPreview() {
+	NewTaskAppBar(navigationToListScreen = {})
 }

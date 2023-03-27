a5cf4111364540f461bfc6e03d6793cc507ebea5
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-27 18:39:16 +0200

    Commit

--- app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskContent.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskContent.kt
@@ -1,20 +1,21 @@
 package com.example.todocomposeapp.ui.screen.task
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.todocomposeapp.R
 import com.example.todocomposeapp.data.model.Priority
 import com.example.todocomposeapp.ui.component.PriorityDropdown
+import com.example.todocomposeapp.ui.theme.taskItemBackgroundColor
+import com.example.todocomposeapp.ui.theme.topAppBarBackgroundColor
 
 @Composable
 fun TaskContent(
@@ -38,6 +39,8 @@ fun TaskContent(
 			singleLine = true,
 			textStyle = MaterialTheme.typography.body1
 		)
+
+		Divider(modifier = Modifier.height(8.dp), color = MaterialTheme.colors.background)
 
 		PriorityDropdown(priority = priority, onPrioritySelected = onPrioritySelected)
 

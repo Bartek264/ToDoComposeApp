0f00e5362a4be81bb32e5698d6ef37769a28e4b5
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-27 18:35:45 +0200

    Commit

--- app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskContent.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskContent.kt
@@ -1,7 +1,20 @@
 package com.example.todocomposeapp.ui.screen.task
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.todocomposeapp.R
 import com.example.todocomposeapp.data.model.Priority
+import com.example.todocomposeapp.ui.component.PriorityDropdown
 
 @Composable
 fun TaskContent(
@@ -12,5 +25,43 @@ fun TaskContent(
 	priority: Priority,
 	onPrioritySelected: (Priority) -> Unit
 ) {
+	Column(
+		Modifier
+			.fillMaxWidth()
+			.padding(16.dp)
+	) {
+		OutlinedTextField(
+			modifier = Modifier.fillMaxWidth(),
+			value = title,
+			onValueChange = { onTitleChange(it) },
+			label = { Text(text = stringResource(id = R.string.title)) },
+			singleLine = true,
+			textStyle = MaterialTheme.typography.body1
+		)
 
+		PriorityDropdown(priority = priority, onPrioritySelected = onPrioritySelected)
+
+		OutlinedTextField(
+			modifier = Modifier.fillMaxSize(),
+			value = description,
+			onValueChange = { onDescriptionChange(it) },
+			label = { Text(text = stringResource(id = R.string.description)) },
+			singleLine = true,
+			textStyle = MaterialTheme.typography.body1
+		)
+
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewTaskContent() {
+	TaskContent(
+		title = "Tile",
+		onTitleChange = {},
+		description = "",
+		onDescriptionChange = {},
+		priority = Priority.LOW,
+		onPrioritySelected = {}
+	)
 }

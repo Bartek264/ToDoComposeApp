023e939f5f5d474e5f2f079bcbcb2db3cdd4bc33
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-28 22:59:33 +0200

    Commit

--- app/src/main/java/com/example/todocomposeapp/ui/screen/list/ListContent.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/screen/list/ListContent.kt
@@ -1,16 +1,21 @@
 package com.example.todocomposeapp.ui.screen.list
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -19,8 +24,10 @@ import androidx.compose.ui.unit.dp
 import com.example.todocomposeapp.data.model.Priority
 import com.example.todocomposeapp.data.model.ToDoEntity
 import com.example.todocomposeapp.ui.component.EmptyComponent
+import com.example.todocomposeapp.ui.theme.HighTaskPriority
 import com.example.todocomposeapp.ui.theme.taskItemBackgroundColor
 import com.example.todocomposeapp.ui.theme.taskItemTextColor
+import com.example.todocomposeapp.utils.Action
 import com.example.todocomposeapp.utils.RequestState
 import com.example.todocomposeapp.utils.SearchAppBarState
 
@@ -33,7 +40,8 @@ fun ListContent(
 	lowPriorityTasks: List<ToDoEntity>,
 	highPriorityTasks: List<ToDoEntity>,
 	searchAppBarState: SearchAppBarState,
-	navigateToTaskScreen: (taskId: Long) -> Unit
+	navigateToTaskScreen: (taskId: Long) -> Unit,
+	onSwipeToDelete: (Action, ToDoEntity) -> Unit
 ) {
 	if (sortState is RequestState.Success) {
 		when {
@@ -41,6 +49,7 @@ fun ListContent(
 				if (searchedTasks is RequestState.Success) {
 					HandleListContent(
 						tasks = searchedTasks.data,
+						onSwipeToDelete = onSwipeToDelete,
 						navigateToTaskScreen = navigateToTaskScreen
 					)
 				}
@@ -49,6 +58,7 @@ fun ListContent(
 				if (allTasks is RequestState.Success) {
 					HandleListContent(
 						tasks = allTasks.data,
+						onSwipeToDelete = onSwipeToDelete,
 						navigateToTaskScreen = navigateToTaskScreen
 					)
 				}
@@ -56,12 +66,14 @@ fun ListContent(
 			sortState.data == Priority.LOW -> {
 				HandleListContent(
 					tasks = lowPriorityTasks,
+					onSwipeToDelete = onSwipeToDelete,
 					navigateToTaskScreen = navigateToTaskScreen
 				)
 			}
 			sortState.data == Priority.HIGH -> {
 				HandleListContent(
 					tasks = highPriorityTasks,
+					onSwipeToDelete = onSwipeToDelete,
 					navigateToTaskScreen = navigateToTaskScreen
 				)
 			}
@@ -71,22 +83,67 @@ fun ListContent(
 
 @ExperimentalMaterialApi
 @Composable
-fun HandleListContent(tasks: List<ToDoEntity>, navigateToTaskScreen: (taskId: Long) -> Unit) {
+fun HandleListContent(
+	tasks: List<ToDoEntity>,
+	onSwipeToDelete: (Action, ToDoEntity) -> Unit,
+	navigateToTaskScreen: (taskId: Long) -> Unit
+) {
 	if (tasks.isEmpty()) {
 		EmptyComponent()
 	} else {
-		DisplayItems(toDoList = tasks, navigateToTaskScreen = navigateToTaskScreen)
+		DisplayItems(
+			toDoList = tasks,
+			onSwipeToDelete = onSwipeToDelete,
+			navigateToTaskScreen = navigateToTaskScreen
+		)
 	}
 }
 
 @ExperimentalMaterialApi
 @Composable
-fun DisplayItems(toDoList: List<ToDoEntity>, navigateToTaskScreen: (taskId: Long) -> Unit) {
+fun DisplayItems(
+	toDoList: List<ToDoEntity>,
+	onSwipeToDelete: (Action, ToDoEntity) -> Unit,
+	navigateToTaskScreen: (taskId: Long) -> Unit
+) {
 	LazyColumn {
 		items(items = toDoList,
 			key = { task -> task.id ?: 0 }) { item: ToDoEntity ->
-			TaskItem(toDoEntity = item, navigateToTaskScreen = navigateToTaskScreen)
+			val dismissState = rememberDismissState()
+			val degrees by animateFloatAsState(
+				targetValue = if (dismissState.targetValue == DismissValue.Default)
+					0f
+				else
+					-45f
+			)
+
+			SwipeToDismiss(
+				state = dismissState,
+				directions = setOf(DismissDirection.EndToStart),
+				dismissThresholds = { FractionalThreshold(0.2f) },
+				background = { RedBackground(degree = degrees) }
+			) {
+				TaskItem(toDoEntity = item, navigateToTaskScreen = navigateToTaskScreen)
+			}
 		}
+	}
+}
+
+@Composable
+fun RedBackground(degree: Float) {
+	Box(
+		modifier = Modifier
+			.fillMaxSize()
+			.background(HighTaskPriority)
+			.padding(horizontal = 24.dp),
+		contentAlignment = Alignment.CenterEnd
+	) {
+		Icon(
+			modifier = Modifier.rotate(degree),
+			imageVector = Icons.Filled.Delete,
+			contentDescription = null,
+			tint = Color.White
+		)
 	}
 }
 

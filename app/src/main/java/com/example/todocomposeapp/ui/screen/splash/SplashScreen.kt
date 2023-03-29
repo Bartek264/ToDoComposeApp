e4556d7143755bb887363d51f2466d927ed5db3c
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-29 20:39:43 +0200

    Commit

--- app/src/main/java/com/example/todocomposeapp/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/screen/splash/SplashScreen.kt
@@ -1,17 +1,22 @@
 package com.example.todocomposeapp.ui.screen.splash
 
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todocomposeapp.R
+import com.example.todocomposeapp.ui.theme.ToDoComposeAppTheme
 import com.example.todocomposeapp.ui.theme.splashScreenBackground
 
 @Composable
@@ -22,9 +27,27 @@ fun SplashScreen() {
 			.background(color = MaterialTheme.colors.splashScreenBackground),
 		contentAlignment = Alignment.Center
 	) {
-		Icon(imageVector = ImageVector.vectorResource(id = getSplashIcon()), contentDescription = null)
+		Image(
+			modifier = Modifier.size(120.dp),
+			painter = painterResource(id = getSplashIcon()),
+			contentDescription = null
+		)
 	}
 }
 
 @Composable
 fun getSplashIcon() = if (isSystemInDarkTheme()) R.drawable.logo_light else R.drawable.logo_dark
+
+@Preview(showSystemUi = true)
+@Composable
+fun SplashScreenPreview() {
+	SplashScreen()
+}
+
+@Composable
+@Preview
+fun SplashScreenPreviewDark() {
+	ToDoComposeAppTheme(darkTheme = true) {
+		SplashScreen()
+	}
+}

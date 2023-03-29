f2884b3e1935de1c7ffa0816dac0068b1c026449
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-29 21:06:37 +0200

    Commit

--- app/src/main/java/com/example/todocomposeapp/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/screen/splash/SplashScreen.kt
@@ -1,16 +1,20 @@
 package com.example.todocomposeapp.ui.screen.splash
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,7 +26,19 @@ import kotlinx.coroutines.delay
 @Composable
 fun SplashScreen(navToListScreen: () -> Unit) {
 
+	/* SplashScreen Animation */
+	var startAnimation by remember { mutableStateOf(false) }
+	val offsetState by animateDpAsState(
+		targetValue = if (startAnimation) 0.dp else 100.dp,
+		animationSpec = tween(durationMillis = 1000)
+	)
+	val alphaState by animateFloatAsState(
+		targetValue = if (startAnimation) 1f else 0f,
+		animationSpec = tween(durationMillis = 1000)
+	)
+
 	LaunchedEffect(key1 = true) {
+		startAnimation = true
 		delay(3000)
 		navToListScreen()
 	}
@@ -34,7 +50,9 @@ fun SplashScreen(navToListScreen: () -> Unit) {
 		contentAlignment = Alignment.Center
 	) {
 		Image(
-			modifier = Modifier.size(120.dp),
+			modifier = Modifier.size(120.dp)
+				.offset(y = offsetState)
+				.alpha(alphaState),
 			painter = painterResource(id = getSplashIcon()),
 			contentDescription = null
 		)

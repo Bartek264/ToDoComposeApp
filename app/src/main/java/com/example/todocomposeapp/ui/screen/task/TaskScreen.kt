091f3e4eaa205a80d746cbc80accde6ca2c49f8e
Bartlomiej Siewierski <bartlomiej.siewierski@wp.pl>
2023-03-30 22:29:21 +0200

    Commit

--- app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskScreen.kt
+++ app/src/main/java/com/example/todocomposeapp/ui/screen/task/TaskScreen.kt
@@ -2,8 +2,11 @@ package com.example.todocomposeapp.ui.screen.task
 
 import android.content.Context
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import com.example.todocomposeapp.data.model.ToDoEntity
 import com.example.todocomposeapp.utils.Action
@@ -17,6 +20,8 @@ fun TaskScreen(
 ) {
 
 	val context = LocalContext.current
+
+	BackHandler(onBackClick = { navigateToListScreen(Action.NO_ACTION) })
 
 	Scaffold(
 		topBar = {
@@ -47,4 +52,31 @@ fun TaskScreen(
 
 fun displayToast(context: Context) {
 	Toast.makeText(context, "Empty fields", Toast.LENGTH_SHORT).show()
+}
+
+/**
+ * @param backDispatcher interception of back button presses on a mobile device. If no dispatcher is passed, the dispatcher from the local context will be used
+ */
+@Composable
+fun BackHandler(
+	backDispatcher: OnBackPressedDispatcher? = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+	onBackClick: () -> Unit
+) {
+	val currentBackPressed by rememberUpdatedState(newValue = onBackClick)
+
+	val backCallback = remember {
+		object : OnBackPressedCallback(true) {
+			override fun handleOnBackPressed() {
+				currentBackPressed()
+			}
+		}
+	}
+
+	DisposableEffect(key1 = backCallback) {
+		// On screen start
+		backDispatcher?.addCallback(backCallback)
+		// On screen destroy
+		onDispose { backCallback.remove() }
+	}
+
 }
